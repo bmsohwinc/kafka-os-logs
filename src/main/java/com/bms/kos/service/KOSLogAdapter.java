149664799a68ebe36313def0f1b9b730b3f30229
Sourabha Bharadwaj BM <bharasou58@gmail.com>
2024-03-01 10:13:44 +0530

    Replace BufferedReader with Scanner to enable file updates to be captured

--- src/main/java/com/bms/kos/service/KOSLogAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.bms.kos.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KOSLogAdapter {
-    
-    
-}
--- src/main/java/com/bms/kos/service/KOSService.java
+++ src/main/java/com/bms/kos/service/KOSService.java
@@ -1,9 +1,12 @@
 package com.bms.kos.service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -22,13 +25,14 @@ public class KOSService {
     public String readSysLogs() throws IOException {
         int numRecordsInserted = 0;
         String kafkaReturnValue = null;
-        try (InputStream iStream = getClass().getClassLoader().getResourceAsStream(syslogPath);
-                BufferedReader br = new BufferedReader(new InputStreamReader(iStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                kafkaReturnValue = kosProducer.sendMessage(line);
+        
+        File file = new File(syslogPath);
+        try (Scanner input = new Scanner(file)) {
+            while (input.hasNextLine()) {
+                kafkaReturnValue = kosProducer.sendMessage(input.nextLine());
                 numRecordsInserted++;
             }
+            input.close();
         }
         System.out.println("No. of logs inserted: " + numRecordsInserted);
         return kafkaReturnValue;
